6fad86b4eecb09a41ea1dd27bccd940b871f323d
Ryan Field <fieldryan19@gmail.com>
2021-12-24 00:39:04 +1300

    [RFVH-1] Created project for backend

---
+++ backend/rfvh-backend/src/test/java/field/ryan/rfvhbackend/RfvhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package field.ryan.rfvhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfvhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
