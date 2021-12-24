9b22848d57e776096059395c7b6b03e98ac6777d
Ryan Field <fieldryan19@gmail.com>
2021-12-24 19:37:25 +1300

    [RFVH-4] Created example connection between frontend and backend

---
+++ backend/rfvh-backend/src/main/java/UserRepositoryImpl.java
@@ -0,0 +1,88 @@
+
+// import java.util.Optional;
+
+// import org.springframework.stereotype.Repository;
+
+// import field.ryan.rfvhbackend.repositories.User;
+// import field.ryan.rfvhbackend.repositories.UserRepository;
+
+// @Repository
+// public class UserRepositoryImpl implements UserRepository {
+
+// @Override
+// public <S extends field.ryan.rfvhbackend.repositories.User> S save(S entity)
+// {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public <S extends field.ryan.rfvhbackend.repositories.User> Iterable<S>
+// saveAll(Iterable<S> entities) {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public Optional<field.ryan.rfvhbackend.repositories.User> findById(Long id) {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public boolean existsById(Long id) {
+// // TODO Auto-generated method stub
+// return false;
+// }
+
+// @Override
+// public Iterable<field.ryan.rfvhbackend.repositories.User> findAll() {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public Iterable<field.ryan.rfvhbackend.repositories.User>
+// findAllById(Iterable<Long> ids) {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public long count() {
+// // TODO Auto-generated method stub
+// return 0;
+// }
+
+// @Override
+// public void deleteById(Long id) {
+// // TODO Auto-generated method stub
+
+// }
+
+// @Override
+// public void delete(field.ryan.rfvhbackend.repositories.User entity) {
+// // TODO Auto-generated method stub
+
+// }
+
+// @Override
+// public void deleteAllById(Iterable<? extends Long> ids) {
+// // TODO Auto-generated method stub
+
+// }
+
+// @Override
+// public void deleteAll(Iterable<? extends
+// field.ryan.rfvhbackend.repositories.User> entities) {
+// // TODO Auto-generated method stub
+
+// }
+
+// @Override
+// public void deleteAll() {
+// // TODO Auto-generated method stub
+
+// }
+
+// }
--- backend/rfvh-backend/src/main/java/field/ryan/rfvhbackend/RFVHBackendApplication.java
+++ backend/rfvh-backend/src/main/java/field/ryan/rfvhbackend/RFVHBackendApplication.java
@@ -1,13 +1,31 @@
 package field.ryan.rfvhbackend;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import field.ryan.rfvhbackend.repositories.User;
+import field.ryan.rfvhbackend.repositories.UserRepository;
 
 @SpringBootApplication
 public class RFVHBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RFVHBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
