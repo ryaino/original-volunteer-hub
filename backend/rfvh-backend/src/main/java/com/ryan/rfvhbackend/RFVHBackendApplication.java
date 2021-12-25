266d8234a38545b8562e0e5057653ba06e0a5370
Ryan Field <fieldryan19@gmail.com>
2021-12-25 21:33:36 +1300

    [RFVH-13] Frontend now generates jwt token

--- backend/rfvh-backend/src/main/java/com/ryan/rfvhbackend/RFVHBackendApplication.java
+++ backend/rfvh-backend/src/main/java/com/ryan/rfvhbackend/RFVHBackendApplication.java
@@ -16,20 +16,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RFVHBackendApplication {
 
 	public static void main(String[] args) throws IOException {
-		ClassLoader classLoader = RFVHBackendApplication.class.getClassLoader();
-
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
-
-		FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://red-frogs-volunteer-hub.firebaseio.com/")
-				.build();
-
-		if (FirebaseApp.getApps().isEmpty()) {
-			FirebaseApp.initializeApp(options);
-		}
-
 		SpringApplication.run(RFVHBackendApplication.class, args);
 	}
 
---
+++ backend/rfvh-backend/src/main/java/com/ryan/rfvhbackend/config/FirebaseAuthConfig.java
@@ -0,0 +1,42 @@
+package com.ryan.rfvhbackend.config;
+
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+/**
+ * Sets up the firebase connection for the app
+ *
+ * @author Ryan Field (fieldryan19@gmail.com)
+ */
+@Configuration
+public class FirebaseAuthConfig {
+
+    @Value("classpath:serviceAccountKey.json")
+    private Resource serviceAccount;
+
+    @Bean
+    FirebaseAuth firebaseAuth() throws IOException {
+        var options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .build();
+
+        var firebaseApp = FirebaseApp.initializeApp(options);
+
+        return FirebaseAuth.getInstance(firebaseApp);
+        // if (FirebaseApp.getApps().isEmpty()) {
+        // FirebaseApp.initializeApp(options);
+        // }
+
+    }
+
+}
